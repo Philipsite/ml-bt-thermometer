d23767a1eb6da2c5d26ff13aab67000d4144b9c4
Philipsite <philip.hartmeier@hotmail.com>
2025-06-25 17:02:36 +0200

    Fix pip upgrade

--- install.bat
+++ install.bat
@@ -6,7 +6,7 @@ echo Installing dependencies...
 :: Activate virtual environment
 call .venv\Scripts\activate.bat
 :: Upgrade pip
-pip install --upgrade pip
+.venv\Scripts\python.exe -m pip install --upgrade pip
 :: Install required packages
 pip install -r requirements.txt
 echo Dependencies installed.
